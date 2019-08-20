2bc363810a3d5affafa7c392fbb968b3a5a67b76
unknown <garreffd@windsor.ca>
2019-08-20 17:24:27 -0400

    Initial project structure

---
+++ src/main/java/com/example/todorestful/TodoRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.todorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todorestful/TodoRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.todorestful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
