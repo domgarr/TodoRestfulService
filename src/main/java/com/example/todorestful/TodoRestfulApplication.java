9c5f1fe74a90d7faec0e8dc19f30f4d93830885c
unknown <garreffd@uwindsor.ca>
2020-01-06 22:46:52 -0500

    Add build setp to build Jar

--- src/main/java/com/example/todorestful/TodoRestfulApplication.java
+++ src/main/java/com/example/todorestful/TodoRestfulApplication.java
@@ -1,17 +1,9 @@
 package com.example.todorestful;
 
-import java.util.Collections;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class TodoRestfulApplication {
